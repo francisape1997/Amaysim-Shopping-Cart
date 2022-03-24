55b0356b68589ac1fdf1ce1a015b4c1c9f2d3856
Francis <90559251+francisape1997@users.noreply.github.com>
2022-03-25 00:56:00 +0800

    Add products

---
+++ src/products.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        "productCode": "ult_small",
+        "productName": "Unlimited 1GB",
+        "price": 24.90,
+        "currency": "USD"
+    },
+    {
+        "productCode": "ult_medium",
+        "productName": "Unlimited 2GB",
+        "price": 29.90,
+        "currency": "USD"
+    },
+    {
+        "productCode": "ult_large",
+        "productName": "Unlimited 5GB",
+        "price": 44.90,
+        "currency": "USD"
+    },
+    {
+        "productCode": "1gb",
+        "productName": "1 GB Data-pack",
+        "price": 9.90,
+        "currency": "USD"
+    }
+];
