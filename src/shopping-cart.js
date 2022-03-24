79f6b0841993362ab2948568468cd52e8a76669b
Francis <90559251+francisape1997@users.noreply.github.com>
2022-03-25 00:55:50 +0800

    Add shopping cart

---
+++ src/shopping-cart.js
@@ -0,0 +1,67 @@
+import { roundOffTwoDecimalPlaces } from "./utilities.js";
+import promoCodes from "./promo-codes.js";
+
+let promoCodeApplied = null;
+
+export default class ShoppingCart {
+
+    constructor(pricingRules = null) {
+        this.pricingRules = pricingRules;
+    };
+
+    items = [];
+
+    get total () {
+        let output = 0;
+
+        for (const item of this.items) {
+            output += item.price;
+        }
+
+        if (this.isPricingRulesApplicable()) {
+            output = this.applyPricingRules();
+        }
+
+        if (promoCodeApplied) {
+            return this.applyPromo(promoCodeApplied, output);
+        }
+
+        return output;
+    };
+
+    add(item, promoCode = null) {
+        this.items.push(item);
+
+        if (promoCode) {
+            this.setPromoCode(promoCode);
+        }
+    };
+
+    setPromoCode(promoCode) {
+        promoCodeApplied = this.validatePromoCode(promoCode);
+    };
+
+    validatePromoCode(promoCode) {
+        const foundPromoCode = promoCodes.find(item => item.name === promoCode);
+
+        if (!foundPromoCode) {
+            console.error('Invalid promo code');
+
+            return false;
+        }
+
+        return foundPromoCode;
+    };
+
+    applyPromo(promoCode, total) {
+        return roundOffTwoDecimalPlaces(total - (total * promoCode.discount));
+    };
+
+    isPricingRulesApplicable() {
+        return this.pricingRules !== null && typeof this.pricingRules === 'function';
+    };
+
+    applyPricingRules() {
+        return this.pricingRules(this.items);
+    };
+}
